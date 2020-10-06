f11b25f3137871c97da2c1f2a0842e2bcfc27ad3
ngecu <devngecu@gmail.com>
2020-10-06 15:54:45 +0300

    final changes

--- frontend.py
+++ frontend.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-from tkinter.filedialog import askopenfile
+
 
 from PIL import Image,ImageTk
 import backend_draft
@@ -116,9 +116,11 @@ def search_email(l,to_text,subject_text,message_text):
             l.delete(0,0)
 def close_sent_window(window):
     window.destroy()
+
+sent_window = None
                
 #functions to call other windows
-def sent_window_f():
+def sent_window_f():   
     sent_window = Toplevel(main_window)
     sent_window.title ("Sent Emails")
     sent_window.geometry("700x200+350+200")
@@ -161,7 +163,7 @@ def sent_window_f():
     close_sent.grid(row=4,column=5)  
     
     
-    listbox1=Listbox(sent_window,height=8,width=60)
+    listbox1=Listbox(sent_window,height=8,width=80)
     listbox1.grid(row=2,column=0,rowspan=10,columnspan=5)    
     
                 
@@ -195,7 +197,7 @@ def draft_window_f():
     e2.grid(row = 0, column = 5, pady = 1) 
 
 
-    listbox1=Listbox(draft_window,height=8,width=60)
+    listbox1=Listbox(draft_window,height=8,width=80)
     listbox1.grid(row=2,column=0,rowspan=10,columnspan=5)    
 
     view_all_btn = Button(draft_window,text="View All",width=12,command=lambda :view_command_draft(listbox1))
@@ -208,9 +210,14 @@ def draft_window_f():
     close_btn = Button(draft_window,text="Close",command=lambda : close_draft_window(draft_window),width=12)
     close_btn.grid(row=4,column=5)  
     
-    
+
+def compose_new():
+    to_email_address_input_field.delete(0,END)
+    subject_input_field.delete(0,END)
+    compose_field.delete('1.0',END)
+
 #background image
-image = Image.open('Assets/original2.png')
+image = Image.open('Assets/background.png')
 photo_image = ImageTk.PhotoImage(image)
 label = Label(main_window, image = photo_image)
 label.pack()
@@ -219,14 +226,13 @@ label.pack()
 
 
 
-compose_var = StringVar()
-compose_label = Label(main_window,textvariable=compose_var,bg="#303841",fg="white",font = ('courier', 30, 'bold') )
-compose_var.set("✍Compose")
-compose_label.place(relx=.01,rely=.4,relwidth=.18,relheight=.075)
+# compose_var = StringVar()
+# compose_label = Label(main_window,textvariable=compose_var,bg="#303841",fg="white",font = ('courier', 30, 'bold') )
+# compose_var.set("✍Compose")
+# compose_label.place(relx=.01,rely=.4,relwidth=.18,relheight=.075)
 
-# attachment_btn = Button(main_window,text="Attachment",command=lambda:UploadAction(),relief=FLAT,bg="#303841",fg="white",font = ('courier', 30, 'bold') )
-# attachment_btn.place(relx=.01,rely=.4,relwidth=.18,relheight=.075)
-
+compose_btn = Button(main_window,text="✍Compose",command=lambda:compose_new(),relief=FLAT,bg="#303841",fg="white",font = ('courier', 30, 'bold') )
+compose_btn.place(relx=.01,rely=.4,relwidth=.18,relheight=.075)
 
 sent_btn = Button(main_window,text="Sent",command=lambda:sent_window_f(),relief=FLAT,bg="#303841",fg="white",font = ('courier', 30, 'bold') )
 sent_btn.place(relx=.01,rely=.595,relwidth=.18,relheight=.075)
@@ -236,7 +242,7 @@ draft_btn.place(relx=.01,rely=.8,relwidth=.18,relheight=.075)
 
 #main_frame
 compose_frame = Frame(main_window,bg="#D72017")
-compose_frame.place(relx=.199,rely=.13,relwidth=.775,relheight=.86)
+compose_frame.place(relx=.22,rely=.13,relwidth=.775,relheight=.86)
 
 #content inside compose frame
 
@@ -268,12 +274,14 @@ compose_field = Text(compose_frame,fg="white",relief=FLAT,bg="#444444",font = ('
 compose_field.place(relx=.197,rely=.32,relwidth=.78,relheight=.55)
 
 
-save_btn = Button(compose_frame,text="Save ♥",command=lambda : save_to_draft(email_value.get(),subject_value.get(),compose_field.get("1.0",END)), relief=FLAT,bg="#737373",fg="white",font = ('courier', 30, 'bold') )
+save_btn = Button(compose_frame,text="Save ♥",command=lambda : save_to_draft(email_value.get().lower(),subject_value.get(),compose_field.get("1.0",END)), relief=FLAT,bg="#737373",fg="white",font = ('courier', 30, 'bold') )
 save_btn.place(relx=.55,rely=.90,relwidth=.18,relheight=.075)
 
 
-send_btn = Button(compose_frame,text="Send \u2713 ",command=lambda :send_email(email_value.get(),subject_value.get(),compose_field.get("1.0",END)),relief=FLAT,bg="#737373",fg="white",font = ('courier', 30, 'bold') )
+send_btn = Button(compose_frame,text="Send \u2713 ",command=lambda :send_email(email_value.get().lower(),subject_value.get(),compose_field.get("1.0",END)),relief=FLAT,bg="#737373",fg="white",font = ('courier', 30, 'bold') )
 send_btn.place(relx=.797,rely=.90,relwidth=.18,relheight=.075)
 
 
 main_window.mainloop()
+#python -m virtualenv .
+#.\scripts\activate
